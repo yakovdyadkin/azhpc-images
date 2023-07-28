7d5e2f843485ecf09f0342b42f5c74c0bd4d1e49
Matt Fraser <mattfras@microsoft.com>
2023-07-28 10:52:14 -0700

    revert package repo change

--- mariner/mariner-2.x/mariner-2.0-hpc/install_utils.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_utils.sh
@@ -3,9 +3,9 @@ set -ex
 
 # Setup microsoft packages repository for moby
 # Download the repository configuration package
-# curl https://packages.microsoft.com/config/rhel/8/prod.repo > ./microsoft-prod.repo
+curl https://packages.microsoft.com/config/rhel/8/prod.repo > ./microsoft-prod.repo
 # Copy the generated list to the sources.list.d directory
-# cp ./microsoft-prod.repo /etc/yum.repos.d/
+cp ./microsoft-prod.repo /etc/yum.repos.d/
 
 yum repolist
 
