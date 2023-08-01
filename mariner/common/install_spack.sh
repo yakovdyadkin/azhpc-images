225f3927f0feaa3746b5de0b8d2a7a673bb0ac33
Matt Fraser <mattfras@microsoft.com>
2023-08-01 14:32:56 -0700

    remove boto3 reference

--- mariner/common/install_spack.sh
+++ mariner/common/install_spack.sh
@@ -22,7 +22,7 @@ dnf install -y curl \
     python3-pip \
     python3-setuptools \
     unzip
-dnf --enablerepo=ha install -y python3-boto3
+# dnf --enablerepo=ha install -y python3-boto3
 
 ## Environment setup for Component installations using Spack
 # Create a directory to setup an environment
