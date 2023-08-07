d58359fd50a3896dda6ca84fc88be8b608088396
Matt Fraser <mattfras@microsoft.com>
2023-08-07 12:28:16 -0700

    ignore pssh

--- mariner/mariner-2.x/common/install_utils.sh
+++ mariner/mariner-2.x/common/install_utils.sh
@@ -9,17 +9,17 @@ dnf install -y  kernel-headers \
 
 # Install Python 3
 dnf install -y python3
-# ln -fs /usr/bin/python3.8 /usr/bin/python3
+ln -fs /usr/bin/python3.9 /usr/bin/python3.6
 
 # install pssh
-pssh_metadata=$(jq -r '.pssh."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
-pssh_version=$(jq -r '.version' <<< $pssh_metadata)
-pssh_sha256=$(jq -r '.sha256' <<< $pssh_metadata)
-pssh_download_url="https://dl.fedoraproject.org/pub/epel/8/Everything/aarch64/Packages/p/pssh-$pssh_version.el8.noarch.rpm"
-$COMMON_DIR/download_and_verify.sh $pssh_download_url $pssh_sha256
+# pssh_metadata=$(jq -r '.pssh."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
+# pssh_version=$(jq -r '.version' <<< $pssh_metadata)
+# pssh_sha256=$(jq -r '.sha256' <<< $pssh_metadata)
+# pssh_download_url="https://dl.fedoraproject.org/pub/epel/8/Everything/aarch64/Packages/p/pssh-$pssh_version.el8.noarch.rpm"
+# $COMMON_DIR/download_and_verify.sh $pssh_download_url $pssh_sha256
 
-dnf install -y  pssh-$pssh_version.el8.noarch.rpm
-rm -f pssh-$pssh_version.el8.noarch.rpm
+# dnf install -y  pssh-$pssh_version.el8.noarch.rpm
+# rm -f pssh-$pssh_version.el8.noarch.rpm
 
 dnf install -y python3-devel \
     gtk2 \
@@ -30,12 +30,13 @@ dnf install -y python3-devel \
     libnl3-devel \
     rpm-build \
     make \
-    kernel-rpm-macros \
-    tk \
     binutils-devel \
     munge \
     numactl-devel \
     environment-modules
+
+#    kernel-rpm-macros \
+#    tk \
     
 ## Disable kernel updates
 echo "exclude=kernel* kmod*" | tee -a /etc/dnf/dnf.conf
@@ -46,12 +47,12 @@ sed -i "$ s/$/ grub2*/" /etc/dnf/dnf.conf
 
 ## Install dkms from the EPEL repository
 wget -r --no-parent -A "dkms-*.el8.noarch.rpm" https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/d/
-dnf localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/d/dkms-*.el8.noarch.rpm -y
+dnf localinstall -y ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/d/dkms-*.el8.noarch.rpm
 
 ## Install subunit and subunit-devel from EPEL repository
 wget -r --no-parent -A "subunit-*.el8.x86_64.rpm" https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/
-dnf localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/subunit-[0-9].*.el8.x86_64.rpm -y
-dnf localinstall ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/subunit-devel-[0-9].*.el8.x86_64.rpm -y
+dnf localinstall -y ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/subunit-[0-9].*.el8.x86_64.rpm
+dnf localinstall -y ./dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/s/subunit-devel-[0-9].*.el8.x86_64.rpm
 
 # Remove rpm files
 rm -rf ./dl.fedoraproject.org/
