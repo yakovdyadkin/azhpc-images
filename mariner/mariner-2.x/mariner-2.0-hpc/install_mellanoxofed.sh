515314afce785f0a7bb1bd8b8e53ff5ec7ca92f7
Matt Fraser <mattfras@microsoft.com>
2023-08-08 11:55:53 -0700

    try old mofed config

--- mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
@@ -3,25 +3,23 @@ set -ex
 
 mofed_metadata=$(jq -r '.mofed."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
 mofed_version=$(jq -r '.version' <<< $mofed_metadata)
-# mofed_sha256=$(jq -r '.sha256' <<< $mofed_metadata)
-# tarball="MLNX_OFED_LINUX-$mofed_version-rhel8.7-x86_64.tgz"
-# mofed_download_url=https://content.mellanox.com/ofed/MLNX_OFED-$mofed_version/$tarball
-# mofed_folder=$(basename $mofed_download_url .tgz)
+mofed_sha256=$(jq -r '.sha256' <<< $mofed_metadata)
+tarball="MLNX_OFED_LINUX-$mofed_version-rhel8.7-x86_64.tgz"
+mofed_download_url=https://content.mellanox.com/ofed/MLNX_OFED-$mofed_version/$tarball
+mofed_folder=$(basename $mofed_download_url .tgz)
 
-# $COMMON_DIR/download_and_verify.sh $mofed_download_url $mofed_sha256
-# tar zxvf $tarball
+$COMMON_DIR/download_and_verify.sh $mofed_download_url $mofed_sha256
+tar zxvf $tarball
 
-# ./$mofed_folder/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/$KERNEL --add-kernel-support --skip-repo --skip-unsupported-devices-check --without-fw-update --distro rhel8.7
-dnf install -y mlnx-ofa_kernel-$mofed_version
-dnf install -y mlnx-ofa_kernel-modules-$mofed_version
+./$mofed_folder/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/$KERNEL --add-kernel-support --skip-repo --skip-unsupported-devices-check --without-fw-update --distro rhel8.7
 $COMMON_DIR/write_component_version.sh "mofed" $mofed_version
 
 # Restarting openibd
 /etc/init.d/openibd restart
 
 # exclude opensm from updates
-# sed -i "$ s/$/ opensm*/" /etc/dnf/dnf.conf
+sed -i "$ s/$/ opensm*/" /etc/dnf/dnf.conf
 
 # cleanup downloaded files
-# rm -rf *.tgz
-# rm -rf -- */
+rm -rf *.tgz
+rm -rf -- */
