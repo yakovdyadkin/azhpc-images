6824517f259f74ff22eeba2dfb46428158cfc411
Matt Fraser <mattfras@microsoft.com>
2023-08-08 14:39:13 -0700

    fix kernel source folder

--- mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
@@ -7,11 +7,12 @@ mofed_sha256=$(jq -r '.sha256' <<< $mofed_metadata)
 tarball="MLNX_OFED_LINUX-$mofed_version-rhel8.7-x86_64.tgz"
 mofed_download_url=https://content.mellanox.com/ofed/MLNX_OFED-$mofed_version/$tarball
 mofed_folder=$(basename $mofed_download_url .tgz)
+kernel_without_arch="${full_version%.*}"
 
 $COMMON_DIR/download_and_verify.sh $mofed_download_url $mofed_sha256
 tar zxvf $tarball
 
-./$mofed_folder/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/$KERNEL --add-kernel-support --skip-repo --skip-unsupported-devices-check --without-fw-update --distro rhel8.7
+./$mofed_folder/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/$kernel_without_arch --add-kernel-support --skip-repo --skip-unsupported-devices-check --without-fw-update --distro rhel8.7
 $COMMON_DIR/write_component_version.sh "mofed" $mofed_version
 
 # Restarting openibd
