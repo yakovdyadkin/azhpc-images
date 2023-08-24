793ff33e29e3bb6f04c268394ab7d5a173049088
Matt Fraser <mattfras@microsoft.com>
2023-08-24 13:20:54 -0700

    download fedora mofed

--- mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
@@ -4,15 +4,14 @@ set -ex
 mofed_metadata=$(jq -r '.mofed."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
 mofed_version=$(jq -r '.version' <<< $mofed_metadata)
 mofed_sha256=$(jq -r '.sha256' <<< $mofed_metadata)
-tarball="MLNX_OFED_SRC-$mofed_version.tgz"
+tarball="MLNX_OFED_LINUX-$mofed_version-fc32-x86_64.tgz"
 mofed_download_url=https://content.mellanox.com/ofed/MLNX_OFED-$mofed_version/$tarball
 mofed_folder=$(basename $mofed_download_url .tgz)
-kernel_without_arch="${KERNEL%.*}"
-MLNX_OFED_SRC-23.04-1.1.3.0.tgz
+
 $COMMON_DIR/download_and_verify.sh $mofed_download_url $mofed_sha256
 tar zxvf $tarball
 
-./$mofed_folder/install.pl --kernel $KERNEL --kernel-sources /usr/src/linux-headers-$kernel_without_arch --add-kernel-support --skip-repo --skip-unsupported-devices-check --without-fw-update --distro rhel8.7
+./$mofed_folder/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/$KERNEL --add-kernel-support --skip-repo --skip-unsupported-devices-check --without-fw-update --distro rhel8.7
 $COMMON_DIR/write_component_version.sh "mofed" $mofed_version
 
 # Restarting openibd
