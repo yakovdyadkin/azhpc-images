1a02eb03037b8ed9a51b5a698258c43565d9967b
Matt Fraser <mattfras@microsoft.com>
2023-09-12 14:23:38 -0700

    update gcc to 13.1

--- mariner/common/install_amd_libs.sh
+++ mariner/common/install_amd_libs.sh
@@ -22,7 +22,7 @@ spack compiler add $gcc_home
 
 # Install AOCC
 spack add aocc@$aocc_version +license-agreed
-spack add amd-aocl@$aocl_version %gcc@9.2.0
+spack add amd-aocl@$aocl_version %gcc@$gcc_version
 spack concretize -f
 spack install
 
