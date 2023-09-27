a22f094268ceb8a936ace32005c93004f642ed9f
Matt Fraser <mattfras@microsoft.com>
2023-09-27 16:15:11 -0700

    strip down docker install

--- mariner/mariner-2.x/common/install_docker.sh
+++ mariner/mariner-2.x/common/install_docker.sh
@@ -14,41 +14,41 @@ curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-contai
 # curl -s -L https://nvidia.github.io/nvidia-container-runtime/experimental/$distribution/nvidia-container-runtime.list | tee /etc/yum.repos.d/nvidia-container-runtime.list
 
 dnf clean expire-cache
-# Install nvidia-docker package
-dnf install -y nvidia-docker2
 
 # Install NVIDIA container toolkit and mark NVIDIA packages on hold
 dnf install -y nvidia-container-toolkit
 
 # Install NVIDIA container runtime and mark NVIDIA packages on hold
-dnf install -y nvidia-container-runtime
+# dnf install -y nvidia-container-runtime
 # Mark the installed packages on hold to disable updates
-sed -i "$ s/$/ *nvidia-container*/" /etc/dnf/dnf.conf
+# sed -i "$ s/$/ *nvidia-container*/" /etc/dnf/dnf.conf
 
-wget https://raw.githubusercontent.com/NVIDIA/nvidia-docker/master/nvidia-docker
-cp nvidia-docker /bin/
-chmod +x /bin/nvidia-docker
+# wget https://raw.githubusercontent.com/NVIDIA/nvidia-docker/master/nvidia-docker
+# cp nvidia-docker /bin/
+# chmod +x /bin/nvidia-docker
 
-nvidia-ctk runtime configure
+nvidia-ctk runtime configure --runtime=docker
+
+# enable and restart the docker daemon to complete the installation
+systemctl enable docker
+systemctl restart docker
 
 # Working setup can be tested by running a base CUDA container
 # nvidia-docker run -e NVIDIA_VISIBLE_DEVICES=all nvidia/cuda:11.0-base nvidia-smi
 
+nvidia-ctk runtime configure --runtime=containerd
+
 # disabling aufs, btrfs, zfs and devmapper snapshotter plugins
-mkdir -p /etc/containerd
-cat << EOF | tee -a /etc/containerd/config.toml
-disabled_plugins = ["cri", "zfs", "aufs", "btrfs", "devmapper"]
-EOF
+# mkdir -p /etc/containerd
+# cat << EOF | tee -a /etc/containerd/config.toml
+# disabled_plugins = ["cri", "zfs", "aufs", "btrfs", "devmapper"]
+# EOF
 
 # restart containerd service
 systemctl restart containerd
 
 # status of containerd snapshotter plugins
 ctr plugin ls
-
-# enable and restart the docker daemon to complete the installation
-systemctl enable docker
-systemctl restart docker
 
 # Write the docker version to components file
 docker_version=$(nvidia-docker --version | awk -F' ' '{print $3}')
