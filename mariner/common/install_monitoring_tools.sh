07d42c0d756092915e2a3fe6b1f4720f9fb63b96
Matt Fraser <mattfras@microsoft.com>
2023-09-28 14:31:51 -0700

    fix python reference

--- mariner/common/install_monitoring_tools.sh
+++ mariner/common/install_monitoring_tools.sh
@@ -2,7 +2,6 @@
 set -ex
 
 # Dependencies
-ln -fs /usr/bin/python3.8 /usr/bin/python3
 python3 -m pip install --upgrade pip
 # Adding path to sudo user
 sed -i 's/.*secure_path.*/Defaults    secure_path = "\/usr\/local\/sbin:\/usr\/local\/bin:\/sbin:\/bin:\/usr\/sbin:\/usr\/bin\/"/' /etc/sudoers
