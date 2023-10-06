5fae7e1653aae4dfd7fed1f387ebf96ab7bdf81a
Matt Fraser <mattfras@microsoft.com>
2023-10-06 10:39:38 -0700

    fix moneo install

--- mariner/common/install_monitoring_tools.sh
+++ mariner/common/install_monitoring_tools.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # Dependencies
-python3 -m pip install --upgrade pip
+# python3 -m pip install --upgrade pip
 # Adding path to sudo user
 sed -i 's/.*secure_path.*/Defaults    secure_path = "\/usr\/local\/sbin:\/usr\/local\/bin:\/sbin:\/bin:\/usr\/sbin:\/usr\/bin\/"/' /etc/sudoers
 
