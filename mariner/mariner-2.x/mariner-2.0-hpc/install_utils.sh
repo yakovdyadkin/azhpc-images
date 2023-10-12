5c0701cac9746bea39569fe46de37557ca8d312a
Matt Fraser <mattfras@microsoft.com>
2023-10-12 13:27:59 -0700

    clean up getting repos

--- mariner/mariner-2.x/mariner-2.0-hpc/install_utils.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_utils.sh
@@ -2,14 +2,11 @@
 set -ex
 
 # Setup Mariner Extended packages repo
-curl https://packages.microsoft.com/cbl-mariner/2.0/prod/extended/x86_64/config.repo > ./mariner-extended-prod.repo
-cp ./mariner-extended-prod.repo /etc/yum.repos.d/
+curl -s -L https://packages.microsoft.com/cbl-mariner/2.0/prod/extended/x86_64/config.repo  | tee /etc/yum.repos.d/mariner-extended-prod.repo
 
 # Setup microsoft packages repository for moby
 # Download the repository configuration package
-curl https://packages.microsoft.com/config/rhel/8/prod.repo > ./microsoft-prod.repo
-# Copy the generated list to the sources.list.d directory
-cp ./microsoft-prod.repo /etc/yum.repos.d/
+curl -s -L https://packages.microsoft.com/config/rhel/8/prod.repo | tee /etc/yum.repos.d/microsoft-prod.repo
 
 dnf repolist
 
