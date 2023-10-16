13fd29e1ebe64dd6fec817d8a9a4508e0215cf6f
Matt Fraser <mattfras@microsoft.com>
2023-10-16 14:37:01 -0700

    update sed

--- mariner/mariner-2.x/mariner-2.0-hpc/install_nvidia_fabric_manager.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_nvidia_fabric_manager.sh
@@ -6,4 +6,5 @@ nvidia_fabricmanager_metadata=$(jq -r '.nvidia."'"$DISTRIBUTION"'".fabricmanager
 nvidia_fabricmanager_version=$(jq -r '.version' <<< $nvidia_fabricmanager_metadata)
 
 tdnf install -y nvidia-fabric-manager-$nvidia_fabricmanager_version
+sed -i "$ s/$/ nvidia-fabric-manager/" /etc/dnf/dnf.conf
 $COMMON_DIR/write_component_version.sh "nvidia_fabricmanager" $nvidia_fabricmanager_version
