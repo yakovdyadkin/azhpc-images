fa6dbe3b05392907475bde2b630840882b5368f1
Matt Fraser <mattfras@microsoft.com>
2023-11-27 11:46:15 -0800

    switch to bashrc

--- mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+source /etc/bashrc
+
 mofed_metadata=$(jq -r '.mofed."'"$DISTRIBUTION"'"' <<< $COMPONENT_VERSIONS)
 mofed_version=$(jq -r '.version' <<< $mofed_metadata)
 mofed_sha256=$(jq -r '.sha256' <<< $mofed_metadata)
@@ -9,8 +11,6 @@ tarball="MLNX_OFED_SRC-$mofed_version.tgz"
 mofed_download_url=https://azhpcstor.blob.core.windows.net/azhpc-images-store/${tarball}
 mofed_folder=$(basename $mofed_download_url .tgz)
 kernel_without_arch="${KERNEL%.*}"
-
-source /etc/profile
 
 $COMMON_DIR/download_and_verify.sh $mofed_download_url $mofed_sha256
 tar zxvf $tarball
