fb83d302f5c6507c4af0869da498e0d183631a34
Matt Fraser <mattfras@microsoft.com>
2024-01-25 11:45:36 -0800

    change exit code

--- mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
+++ mariner/mariner-2.x/mariner-2.0-hpc/install_mellanoxofed.sh
@@ -13,7 +13,7 @@ $COMMON_DIR/download_and_verify.sh $mofed_download_url $mofed_sha256
 tar zxvf $tarball
 
 gcc --version
-exit 0
+exit 1
 
 pushd $mofed_folder
 ./install.pl --all --without-openmpi --without-mlnx-ofa_kernel-modules
