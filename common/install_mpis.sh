60f819e64e860b898b56382aae58ec8fa26e9da6
yakovdyadkin <yakovdyad@gmail.com>
2024-03-20 10:42:03 -0500

    Update nvidia driver & only install HPCX

--- common/install_mpis.sh
+++ common/install_mpis.sh
@@ -34,10 +34,10 @@ EOF
 # Softlink for HPC-x
 ln -s $module_files_directory/hpcx-$hpcx_version $module_files_directory/hpcx
 
-# Add the MPIs to the environment
-# Install MVAPICH2
-mvapich2_version=$(jq -r '.mvapich2."'"$DISTRIBUTION"'".version' <<< $COMPONENT_VERSIONS)
-spack add mvapich2@$mvapich2_version %gcc@$gcc_version
+# # Add the MPIs to the environment
+# # Install MVAPICH2
+# mvapich2_version=$(jq -r '.mvapich2."'"$DISTRIBUTION"'".version' <<< $COMPONENT_VERSIONS)
+# spack add mvapich2@$mvapich2_version %gcc@$gcc_version
 
 # Add UCX, HCOLL as externals for Open MPI installation
 # /opt/azurehpc/spack/etc/spack/defaults/packages.yaml
@@ -97,95 +97,95 @@ w
 q
 EOF
 
-# Install Open MPI
-ompi_version=$(jq -r '.ompi."'"$DISTRIBUTION"'".version' <<< $COMPONENT_VERSIONS)
-spack add openmpi@$ompi_version fabrics=ucx,hcoll ^ucx@$ucx_version ^hcoll@$hcoll_version %gcc@$gcc_version
+# # Install Open MPI
+# ompi_version=$(jq -r '.ompi."'"$DISTRIBUTION"'".version' <<< $COMPONENT_VERSIONS)
+# spack add openmpi@$ompi_version fabrics=ucx,hcoll ^ucx@$ucx_version ^hcoll@$hcoll_version %gcc@$gcc_version
 
-# Install Intel MPI 2021
-impi_2021_version=$(jq -r '.impi_2021."'"$DISTRIBUTION"'".version' <<< $COMPONENT_VERSIONS)
-spack add intel-oneapi-mpi@$impi_2021_version
+# # Install Intel MPI 2021
+# impi_2021_version=$(jq -r '.impi_2021."'"$DISTRIBUTION"'".version' <<< $COMPONENT_VERSIONS)
+# spack add intel-oneapi-mpi@$impi_2021_version
 
-# Install the MPIs
-spack concretize -f
-spack install
+# # Install the MPIs
+# spack concretize -f
+# spack install
 
-# Set the installation directories
-mvapich2_path=$(spack location -i mvapich2@$mvapich2_version)
-ompi_path=$(spack location -i openmpi@$ompi_version)
-impi_2021_path=$(spack location -i intel-oneapi-mpi@$impi_2021_version)/mpi/$impi_2021_version
-mv $(echo $impi_2021_path)/modulefiles/mpi $(echo $impi_2021_path)/modulefiles/impi
+# # Set the installation directories
+# mvapich2_path=$(spack location -i mvapich2@$mvapich2_version)
+# ompi_path=$(spack location -i openmpi@$ompi_version)
+# impi_2021_path=$(spack location -i intel-oneapi-mpi@$impi_2021_version)/mpi/$impi_2021_version
+# mv $(echo $impi_2021_path)/modulefiles/mpi $(echo $impi_2021_path)/modulefiles/impi
 
-# MVAPICH2
-cat << EOF >> $module_files_directory/mvapich2-$mvapich2_version
-#%Module 1.0
-#
-#  MVAPICH2 $mvapich2_version
-#
-conflict        mpi
+# # MVAPICH2
+# cat << EOF >> $module_files_directory/mvapich2-$mvapich2_version
+# #%Module 1.0
+# #
+# #  MVAPICH2 $mvapich2_version
+# #
+# conflict        mpi
 
-if { "$DISTRIBUTION" == "almalinux8.7" } {
-    setenv gcc_version $gcc_version
-    module load gcc-$gcc_version
-}
+# if { "$DISTRIBUTION" == "almalinux8.7" } {
+#     setenv gcc_version $gcc_version
+#     module load gcc-$gcc_version
+# }
 
-prepend-path    PATH            $(echo $mvapich2_path)/bin
-prepend-path    LD_LIBRARY_PATH $(echo $mvapich2_path)/lib
-prepend-path    MANPATH         $(echo $mvapich2_path)/share/man
-setenv          MPI_BIN         $(echo $mvapich2_path)/bin
-setenv          MPI_INCLUDE     $(echo $mvapich2_path)/include
-setenv          MPI_LIB         $(echo $mvapich2_path)/lib
-setenv          MPI_MAN         $(echo $mvapich2_path)/share/man
-setenv          MPI_HOME        $(echo $mvapich2_path)
-EOF
+# prepend-path    PATH            $(echo $mvapich2_path)/bin
+# prepend-path    LD_LIBRARY_PATH $(echo $mvapich2_path)/lib
+# prepend-path    MANPATH         $(echo $mvapich2_path)/share/man
+# setenv          MPI_BIN         $(echo $mvapich2_path)/bin
+# setenv          MPI_INCLUDE     $(echo $mvapich2_path)/include
+# setenv          MPI_LIB         $(echo $mvapich2_path)/lib
+# setenv          MPI_MAN         $(echo $mvapich2_path)/share/man
+# setenv          MPI_HOME        $(echo $mvapich2_path)
+# EOF
 
-# OpenMPI
-cat << EOF >> $module_files_directory/openmpi-$ompi_version
-#%Module 1.0
-#
-#  OpenMPI $ompi_version
-#
-conflict        mpi
+# # OpenMPI
+# cat << EOF >> $module_files_directory/openmpi-$ompi_version
+# #%Module 1.0
+# #
+# #  OpenMPI $ompi_version
+# #
+# conflict        mpi
 
-if { "$DISTRIBUTION" == "almalinux8.7" } {
-    setenv gcc_version $gcc_version
-    module load gcc-$gcc_version
-}
+# if { "$DISTRIBUTION" == "almalinux8.7" } {
+#     setenv gcc_version $gcc_version
+#     module load gcc-$gcc_version
+# }
 
-prepend-path    PATH            $(echo $ompi_path)/bin
-prepend-path    LD_LIBRARY_PATH $(echo $ompi_path)/lib
-prepend-path    MANPATH         $(echo $ompi_path)/share/man
-setenv          MPI_BIN         $(echo $ompi_path)/bin
-setenv          MPI_INCLUDE     $(echo $ompi_path)/include
-setenv          MPI_LIB         $(echo $ompi_path)/lib
-setenv          MPI_MAN         $(echo $ompi_path)/share/man
-setenv          MPI_HOME        $(echo $ompi_path)
-EOF
+# prepend-path    PATH            $(echo $ompi_path)/bin
+# prepend-path    LD_LIBRARY_PATH $(echo $ompi_path)/lib
+# prepend-path    MANPATH         $(echo $ompi_path)/share/man
+# setenv          MPI_BIN         $(echo $ompi_path)/bin
+# setenv          MPI_INCLUDE     $(echo $ompi_path)/include
+# setenv          MPI_LIB         $(echo $ompi_path)/lib
+# setenv          MPI_MAN         $(echo $ompi_path)/share/man
+# setenv          MPI_HOME        $(echo $ompi_path)
+# EOF
 
-# Intel 2021
-cat << EOF >> $module_files_directory/impi_$impi_2021_version
-#%Module 1.0
-#
-#  Intel MPI $impi_2021_version
-#
-conflict        mpi
-module load $(echo $impi_2021_path)/modulefiles/impi
-setenv          MPI_BIN         $(echo $impi_2021_path)/bin
-setenv          MPI_INCLUDE     $(echo $impi_2021_path)/include
-setenv          MPI_LIB         $(echo $impi_2021_path)/lib
-setenv          MPI_MAN         $(echo $impi_2021_path)/man
-setenv          MPI_HOME        $(echo $impi_2021_path)
-EOF
+# # Intel 2021
+# cat << EOF >> $module_files_directory/impi_$impi_2021_version
+# #%Module 1.0
+# #
+# #  Intel MPI $impi_2021_version
+# #
+# conflict        mpi
+# module load $(echo $impi_2021_path)/modulefiles/impi
+# setenv          MPI_BIN         $(echo $impi_2021_path)/bin
+# setenv          MPI_INCLUDE     $(echo $impi_2021_path)/include
+# setenv          MPI_LIB         $(echo $impi_2021_path)/lib
+# setenv          MPI_MAN         $(echo $impi_2021_path)/man
+# setenv          MPI_HOME        $(echo $impi_2021_path)
+# EOF
 
-# Softlinks for MVAPICH2, Open MPI and IMPI 2021
-ln -s $module_files_directory/mvapich2-$mvapich2_version $module_files_directory/mvapich2
-ln -s $module_files_directory/openmpi-$ompi_version $module_files_directory/openmpi
-ln -s $module_files_directory/impi_$impi_2021_version $module_files_directory/impi-2021
+# # Softlinks for MVAPICH2, Open MPI and IMPI 2021
+# ln -s $module_files_directory/mvapich2-$mvapich2_version $module_files_directory/mvapich2
+# ln -s $module_files_directory/openmpi-$ompi_version $module_files_directory/openmpi
+# ln -s $module_files_directory/impi_$impi_2021_version $module_files_directory/impi-2021
 
-# Write MPI component versions
-$COMMON_DIR/write_component_version.sh "hpcx" $hpcx_version
-$COMMON_DIR/write_component_version.sh "mvapich2" $mvapich2_version
-$COMMON_DIR/write_component_version.sh "ompi" $ompi_version
-$COMMON_DIR/write_component_version.sh "impi_2021" $impi_2021_version
+# # Write MPI component versions
+# $COMMON_DIR/write_component_version.sh "hpcx" $hpcx_version
+# $COMMON_DIR/write_component_version.sh "mvapich2" $mvapich2_version
+# $COMMON_DIR/write_component_version.sh "ompi" $ompi_version
+# $COMMON_DIR/write_component_version.sh "impi_2021" $impi_2021_version
 
 # Remove Stale files
 rm -rf /tmp/tmp*
